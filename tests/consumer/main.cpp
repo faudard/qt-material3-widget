4ce6fb2a62d604fcb51fab521ed360947bd28c5a
dad <faudard86@gmail.com>
2026-07-19 18:03:27 +0200

    fix(packaging): export cleanup modules as package components

--- tests/consumer/main.cpp
+++ tests/consumer/main.cpp
@@ -3,12 +3,17 @@
 
 #include <qtmaterial/widgets/buttons/qtmaterialtextbutton.h>
 
+#include <qtmaterial/testing/qtmaterialautomation.h>
+#include <qtmaterial/integration/qtmaterialpaletteadapter.h>
+#include <qtmaterial/tooling/qtmaterialcomponentregistry.h>
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
 
     QtMaterial::QtMaterialTextButton button;
     button.setText(QStringLiteral("Consumer OK"));
+    QtMaterial::QtMaterialAutomation::setTestId(
+        &button, QStringLiteral("installed-consumer"));
 
     return 0;
 }
