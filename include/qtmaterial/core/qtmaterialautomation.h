cfe4a753996b405a90d9ac3cf5c934b9c8288b9b
dad <faudard86@gmail.com>
2026-07-24 09:50:44 +0200

    commit

--- include/qtmaterial/core/qtmaterialautomation.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Transitional source-compatibility include.
-// New test/tooling code should include the testing header directly.
-#include "qtmaterial/testing/qtmaterialautomation.h"
