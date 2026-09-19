35f29058b333a579276a5ed7b81da8d252ce552e
faudard <39192701+faudard@users.noreply.github.com>
2026-09-19 19:06:51 +0200

    release: bump documentation version to 0.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ DOXYGEN_XML_DIR = ROOT_DIR / "build" / "doxygen" / "xml"
 
 project = "qt-material3-widget"
 author = "faudard"
-release = "0.4.1"
+release = "0.5.0"
 
 extensions = [
     "myst_parser",
