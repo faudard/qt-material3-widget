720adc4e68565ec28d01c60c843838f85270f66e
faudard <39192701+faudard@users.noreply.github.com>
2026-09-21 11:30:44 +0200

    feat: add 0.9 desktop productivity components

---
+++ include/qtmaterial/widgets/layouts/qtmaterialsplitview.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QList>
+#include <QSplitter>
+
+#include "qtmaterial/qtmaterialglobal.h"
+
+namespace QtMaterial {
+
+class QTMATERIAL3_WIDGETS_EXPORT QtMaterialSplitView : public QSplitter
+{
+    Q_OBJECT
+
+public:
+    explicit QtMaterialSplitView(QWidget* parent = nullptr);
+    explicit QtMaterialSplitView(Qt::Orientation orientation, QWidget* parent = nullptr);
+
+    void setPaneCollapsible(int index, bool collapsible);
+    bool paneCollapsible(int index) const;
+
+    void setPaneCollapsed(int index, bool collapsed);
+    bool paneCollapsed(int index) const;
+
+Q_SIGNALS:
+    void paneCollapsedChanged(int index, bool collapsed);
+
+private:
+    QList<int> m_lastExpandedSizes;
+};
+
+} // namespace QtMaterial
