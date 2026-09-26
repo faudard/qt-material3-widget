e13fc413a58e93683a4cc7d4daf7a10ea7a164f6
faudard <39192701+faudard@users.noreply.github.com>
2026-09-26 12:31:22 +0200

    api: internalize autocomplete popup declaration

--- src/widgets/inputs/private/qtmaterialautocompletepopup_p.h
+++ src/widgets/inputs/private/qtmaterialautocompletepopup_p.h
@@ -1,21 +1,96 @@
 #pragma once
 
-#include <QPersistentModelIndex>
+#include <memory>
+
 #include <QPointer>
 #include <QStringList>
+#include <QWidget>
 
-class QListView;
-class QSortFilterProxyModel;
-class QStringListModel;
+#include "qtmaterial/qtmaterialglobal.h"
+#include "qtmaterial/theme/qtmaterialthemecontexthost.h"
 
-struct QtMaterialAutocompletePopupPrivate
+class QAbstractItemModel;
+class QEvent;
+class QKeyEvent;
+class QLineEdit;
+class QPaintEvent;
+class QResizeEvent;
+
+namespace QtMaterial {
+struct AutocompletePopupSpec;
+class Theme;
+class ThemeContext;
+} // namespace QtMaterial
+
+class QtMaterialAutocompletePopupPrivate;
+
+class QTMATERIAL3_WIDGETS_EXPORT QtMaterialAutocompletePopup
+    : public QWidget
+    , public QtMaterial::ThemeContextHost
 {
-    QStringListModel* sourceModel = nullptr;
-    QSortFilterProxyModel* proxyModel = nullptr;
-    QListView* view = nullptr;
+    Q_OBJECT
+    Q_INTERFACES(QtMaterial::ThemeContextHost)
+    Q_PROPERTY(
+        QtMaterial::ThemeContext* themeContext
+        READ themeContext
+        WRITE setThemeContext
+        NOTIFY themeContextChanged)
 
-    QString filterText;
-    QPersistentModelIndex currentIndex;
-    bool popupVisible = false;
-    bool geometryDirty = true;
+public:
+    explicit QtMaterialAutocompletePopup(QWidget* parent = nullptr);
+    ~QtMaterialAutocompletePopup() override;
+
+    void setThemeContext(QtMaterial::ThemeContext* context);
+    QtMaterial::ThemeContext* themeContext() const noexcept override;
+    QtMaterial::ThemeContext* effectiveThemeContext() const noexcept override;
+
+    void setAnchorLineEdit(QLineEdit* lineEdit);
+    QLineEdit* anchorLineEdit() const noexcept;
+
+    void setModel(QAbstractItemModel* model);
+    QAbstractItemModel* model() const noexcept;
+
+    void setSuggestions(const QStringList& suggestions);
+    QString currentCompletion() const;
+
+    void setPopupVisible(bool visible);
+    bool isPopupVisible() const noexcept;
+
+    void setFilterText(const QString& text);
+    QString filterText() const;
+
+    void clearSelection();
+    void selectNext();
+    void selectPrevious();
+    void acceptCurrent();
+
+    QSize sizeHint() const override;
+    QSize minimumSizeHint() const override;
+
+Q_SIGNALS:
+    void themeContextChanged(QtMaterial::ThemeContext* context);
+    void effectiveThemeContextChanged(QtMaterial::ThemeContext* context);
+    void completionActivated(const QString& text);
+    void popupVisibilityChanged(bool visible);
+
+protected:
+    bool event(QEvent* event) override;
+    bool eventFilter(QObject* watched, QEvent* event) override;
+    void paintEvent(QPaintEvent* event) override;
+    void resizeEvent(QResizeEvent* event) override;
+    void keyPressEvent(QKeyEvent* event) override;
+
+private:
+    const QtMaterial::AutocompletePopupSpec& resolvedSpec() const;
+    void handleThemeChanged(const QtMaterial::Theme& theme);
+    void refreshPopupVisibility();
+    void setEffectivePopupVisible(bool visible);
+    void ensureSpecResolved() const;
+    void invalidatePopupLayout();
+    void updatePopupGeometry();
+    void updatePopupPalette();
+    void syncToAnchorGeometry();
+    void syncSelectionFromCurrentIndex();
+
+    std::unique_ptr<QtMaterialAutocompletePopupPrivate> d_ptr;
 };
