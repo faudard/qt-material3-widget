c32b2f04274fa7433ebbac61968d5376fba98021
faudard <39192701+faudard@users.noreply.github.com>
2026-09-26 12:32:12 +0200

    test: use private autocomplete popup contract

--- tests/widgets/inputs/tst_autocompletepopup.cpp
+++ tests/widgets/inputs/tst_autocompletepopup.cpp
@@ -4,7 +4,7 @@
 
 #include "qtmaterial/core/qtmaterialwidget.h"
 #include "qtmaterial/theme/qtmaterialthemecontext.h"
-#include "qtmaterial/widgets/inputs/qtmaterialautocompletepopup.h"
+#include "qtmaterialautocompletepopup_p.h"
 
 class tst_QtMaterialAutocompletePopup : public QObject
 {
